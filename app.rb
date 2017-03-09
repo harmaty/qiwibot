efa0412cd17151d206f2e8195f95ef706663a0f7
Artem Harmaty <harmaty@gmail.com>
2017-03-09 18:43:59 +0300

    upper case for host

--- app.rb
+++ app.rb
@@ -3,5 +3,5 @@ load 'lib/server.rb'
 load 'lib/agent.rb'
 load 'lib/sms_messages.rb'
 
-server = Server.new ENV['LOGIN'], ENV['PASSWORD'], ENV['host'] || 'localhost', ENV['PORT'] || 8081
+server = Server.new ENV['LOGIN'], ENV['PASSWORD'], ENV['HOST'] || 'localhost', ENV['PORT'] || 8081
 server.run
