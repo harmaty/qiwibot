9265dafce026d9faa044d0731276ca8bd89413c3
Artem Harmaty <harmaty@gmail.com>
2017-03-20 19:01:49 +0300

    encode request params

--- lib/qiwibot/jwt_auth.rb
+++ lib/qiwibot/jwt_auth.rb
@@ -7,16 +7,24 @@ module Qiwibot
     end
 
     def call env
+      request = Rack::Request.new env
       begin
         bearer = env.fetch('HTTP_AUTHORIZATION', '').slice(7..-1)
         payload, header = JWT.decode bearer, ENV['JWT_SECRET'], true, {algorithm: 'HS256'}
-        env[:operation] = payload['operation']
+        operation = payload['operation']
 
+        unless operation && request.path_info.include?(operation['command']) && operation['params'] == request.params
+          raise 'Invalid token.'
+        end
+
+        env[:operation] = operation
         @app.call env
       rescue JWT::ExpiredSignature
         json_response({message: 'The token has expired.'}, 403)
       rescue JWT::DecodeError
         json_response({message: 'A token must be passed.'}, 401)
+      rescue => e
+        json_response({message: e.message}, 401)
       end
     end
 
